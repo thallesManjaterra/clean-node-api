69d74ef487731fc01855449572b858ec4988c05e
thallesManjaterra <thalles.manjaterra@gmail.com>
2021-03-01 13:59:43 -0300

    refactor: group all errors exports in a single

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,5 +1,4 @@
-import { InvalidParamError } from '../errors/invalid-param-error'
-import { MissingParamError } from '../errors/missing-param-error'
+import { MissingParamError, InvalidParamError } from '../errors'
 import { badRequest, serverError } from '../helpers/http-helper'
 import { EmailValidator } from '../protocols/email-validator'
 import { SignUpController } from './signup'
