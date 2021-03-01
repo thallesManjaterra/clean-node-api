2a77c021a63ccfc4346a8fc03c11832554b14416
thallesManjaterra <thalles.manjaterra@gmail.com>
2021-03-01 19:24:19 -0300

    test: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,24 +1,49 @@
+import { AccountModel } from '../../../domain/models/account'
 import { DbAddAccount } from './db-add-account'
-import { AddAccountModel, Encrypter } from './db-add-account-protocols'
+import { AddAccountModel, Encrypter, AddAccountRepository } from './db-add-account-protocols'
+
+function makeFakeHash (): string {
+  return 'hashed_value'
+}
 
 function makeEncrypter (): Encrypter {
   class EncrypterStub implements Encrypter {
     async encrypt (_value: string): Promise<string> {
-      return await Promise.resolve('hash_value')
+      return await Promise.resolve(makeFakeHash())
     }
   }
   return new EncrypterStub()
 }
 
+function makeFakeAccount (): AccountModel {
+  return {
+    id: 'any_id',
+    name: 'any_name',
+    email: 'any_email',
+    password: 'any_password'
+  }
+}
+
+function makeAddAccountRepository (): AddAccountRepository {
+  class AddAccountRepositoryStub implements AddAccountRepository {
+    async add (_accountData: AddAccountModel): Promise<AccountModel> {
+      return await Promise.resolve(makeFakeAccount())
+    }
+  }
+  return new AddAccountRepositoryStub()
+}
+
 interface SutTypes {
   sut: DbAddAccount
   encrypterStub: Encrypter
+  addAccountRepositoryStub: AddAccountRepository
 }
 
 function makeSut (): SutTypes {
   const encrypterStub = makeEncrypter()
-  const sut = new DbAddAccount(encrypterStub)
-  return { sut, encrypterStub }
+  const addAccountRepositoryStub = makeAddAccountRepository()
+  const sut = new DbAddAccount(encrypterStub, addAccountRepositoryStub)
+  return { sut, encrypterStub, addAccountRepositoryStub }
 }
 
 function makeFakeAccountData (): AddAccountModel {
@@ -41,5 +66,11 @@ describe('DbAddAccount Usecase', () => {
     jest.spyOn(encrypterStub, 'encrypt').mockRejectedValue(new Error())
     const promise = sut.add(makeFakeAccountData())
     await expect(promise).rejects.toThrow()
+  })
+  test('should call AddAccountRepository with correct values', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addAccountRepositoryStub, 'add')
+    await sut.add(makeFakeAccountData())
+    expect(addSpy).toHaveBeenLastCalledWith({ ...makeFakeAccountData(), password: makeFakeHash() })
   })
 })
