68bcdc974fac9c84c83e4c703967aa91dd97a3a5
thallesManjaterra <thalles.manjaterra@gmail.com>
2021-03-03 14:23:43 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,4 +1,5 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 function makeFakeAccountData (): any {
@@ -11,6 +12,16 @@ function makeFakeAccountData (): any {
 }
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
   test('should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
