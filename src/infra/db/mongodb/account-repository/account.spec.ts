bbc301011f4dc475499be8dae1a5ea8eb49944f3
thallesManjaterra <thalles.manjaterra@gmail.com>
2021-03-03 15:31:11 -0300

    test: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -19,7 +19,7 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
   afterAll(async () => {
