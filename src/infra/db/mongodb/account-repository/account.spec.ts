4d129218082728a6fc239ca48e8b7b87a25966d0
thallesManjaterra <thalles.manjaterra@gmail.com>
2021-03-03 12:29:11 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,30 @@
+import { AddAccountModel } from '../../../../domain/usescases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+function makeFakeAccountData (): AddAccountModel {
+  return {
+    name: 'any_name',
+    email: 'any_email',
+    password: 'hashed_password'
+  }
+}
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  test('should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add(makeFakeAccountData())
+    const accountData = makeFakeAccountData()
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe(accountData.name)
+    expect(account.email).toBe(accountData.email)
+    expect(account.password).toBe(accountData.password)
+  })
+})
