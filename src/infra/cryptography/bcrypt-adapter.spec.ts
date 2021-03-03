19ce7730e732cfa8835f40319944191e7caf3a17
thallesManjaterra <thalles.manjaterra@gmail.com>
2021-03-03 11:38:23 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,13 +1,30 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+interface SutTypes {
+  sut: BcryptAdapter
+  salt: number
+}
+
+function makeSut (): SutTypes {
+  const salt = 12
+  const sut = new BcryptAdapter(salt)
+  return { sut, salt }
+}
+
+const ANY_VALUE = 'any_value'
+
 describe('Bcrypt Adapter', () => {
   test('should call bcrypt with correct values', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const { sut, salt } = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    const anyValue = 'any_value'
-    await sut.encrypt(anyValue)
-    expect(hashSpy).toHaveBeenCalledWith(anyValue, salt)
+    await sut.encrypt(ANY_VALUE)
+    expect(hashSpy).toHaveBeenCalledWith(ANY_VALUE, salt)
+  })
+  test('should throw if bcrypt throws', async () => {
+    const { sut } = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockRejectedValueOnce(new Error())
+    const promise = sut.encrypt(ANY_VALUE)
+    await expect(promise).rejects.toThrow()
   })
 })
