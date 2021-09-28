882df77d23b6db2a4b6c78e242546ad14657a03a
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-09-28 13:10:17 -0300

    move sut creation to a factory function

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -3,14 +3,19 @@ const validator = require('validator')
 
 describe('Email Validator', () => {
   test('should return true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
   test('should return false if validator returns false', () => {
     validator.isEmail.mockReturnValueOnce(false)
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@mail.com')
     expect(isEmailValid).toBe(false)
   })
 })
+
+function makeSut () {
+  const sut = new EmailValidator()
+  return sut
+}
