2c0a1c583122c6193efb6fef5f1fd1824cb6dee3
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-09-29 11:45:05 -0300

    ensure AuthUseCase calls LoadUserByEmailRepository with correct email

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -3,11 +3,30 @@ const { MissingParamError } = require('../../utils/errors')
 
 describe('Auth Usecase', () => {
   test('should throw if no email is provided', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     expect(sut.auth()).rejects.toThrow(new MissingParamError('email'))
   })
   test('should throw if no password is provided', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     expect(sut.auth('any_email@mail.com')).rejects.toThrow(new MissingParamError('password'))
   })
+  test('should call LoadUserByEmailRepository with correct email', async () => {
+    const { sut, loadUserByEmailRepositoryMock } = makeSut()
+    await sut.auth('any_email@mail.com', 'any_password')
+    expect(loadUserByEmailRepositoryMock.load).toHaveBeenCalledWith('any_email@mail.com')
+  })
 })
+
+function makeSut () {
+  const loadUserByEmailRepositoryMock = makeLoadUserByEmailRepository()
+  const sut = new AuthUseCase(loadUserByEmailRepositoryMock)
+  return {
+    sut, loadUserByEmailRepositoryMock
+  }
+}
+
+function makeLoadUserByEmailRepository () {
+  return {
+    load: jest.fn(async () => {})
+  }
+}
