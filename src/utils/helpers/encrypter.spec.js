f715f9c6c8e668d85d3e0162cea4d0bd2c6e8045
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-10-01 14:46:56 -0300

    move sut creation to a factory function

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -3,19 +3,24 @@ const bcrypt = require('bcrypt')
 
 describe('Encrypter', () => {
   test('should return true if bcrypt.compare returns true', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     const isValuesMatch = await sut.compare('any_value', 'hashed_value')
     expect(isValuesMatch).toBe(true)
   })
   test('should return false if bcrypt.compare returns false', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     bcrypt.compare.mockResolvedValueOnce(false)
     const isValuesMatch = await sut.compare('any_value', 'hashed_of_another_value')
     expect(isValuesMatch).toBe(false)
   })
   test('should call bcrypt.compare with correct values', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     await sut.compare('any_value', 'hashed_value')
     expect(bcrypt.compare).toHaveBeenCalledWith('any_value', 'hashed_of_another_value')
   })
 })
+
+function makeSut () {
+  const sut = new Encrypter()
+  return sut
+}
