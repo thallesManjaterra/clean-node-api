eb4b67a6737eea6e9f0fb3c55e65d6d2643355a7
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-10-02 09:46:03 -0300

    move sut creation to a factory function

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -3,14 +3,19 @@ const jwt = require('jsonwebtoken')
 
 describe('Token Generator', () => {
   test('should return null if JWT returns null', () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
     jwt.sign.mockReturnValueOnce(null)
     const token = sut.generate('any_id')
     expect(token).toBeNull()
   })
   test('should return a token if JWT returns a token', () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
     const token = sut.generate('any_id')
     expect(token).toBe('any_token')
   })
 })
+
+function makeSut () {
+  const sut = new TokenGenerator()
+  return sut
+}
