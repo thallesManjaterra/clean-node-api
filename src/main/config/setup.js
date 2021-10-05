58a115d1acf0ea6f3cb783b2710addaa1db6998f
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-10-04 23:03:13 -0300

    enable CORS

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,3 +1,10 @@
 module.exports = (app) => {
   app.disable('x-powered-by')
+  app.use((req, res, next) => {
+    res
+      .set('access-control-allow-origin', '*')
+      .set('access-control-allow-methods', '*')
+      .set('access-control-allow-headers', '*')
+    next()
+  })
 }
--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -9,4 +9,14 @@ describe('App Setup', () => {
     const res = await request(app).get('/test_x_powered_by')
     expect(res.headers['x-powered-by']).toBeUndefined()
   })
+
+  test('should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.end()
+    })
+    const res = await request(app).get('/test_cors')
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
+  })
 })
