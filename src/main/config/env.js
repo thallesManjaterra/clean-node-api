8025103fa9832f9e21ac4f2c111be73a0fc829b7
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-10-05 13:38:39 -0300

    add app port to env file

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-js-api',
-  secretKey: process.env.SECRET_KEY || 'secret'
+  secretKey: process.env.SECRET_KEY || 'secret',
+  port: process.env.PORT || 5858
 }
