8ed2af65c3d311153ecc599e2c1357d38576baae
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2021-10-05 09:53:14 -0300

    move LoginRoute composition to its own file

---
+++ src/main/composers/login-route-composer.js
@@ -0,0 +1,20 @@
+const LoginRoute = require('../../presentation/routes/login-route')
+const AuthUseCase = require('../../domain/usecases/auth-usecase')
+const EmailValidator = require('../../utils/helpers/email-validator')
+const Encrypter = require('../../utils/helpers/encrypter')
+const TokenGenerator = require('../../utils/helpers/token-generator')
+const LoadUserByEmailRepository = require('../../infra/repositories/load-user-by-email-repository')
+const UpdateAccessTokenRepository = require('../../infra/repositories/update-access-token-repository')
+const env = require('../config/env')
+
+const loadUserByEmailRepository = new LoadUserByEmailRepository()
+const updateAccessTokenRepository = new UpdateAccessTokenRepository()
+const encrypter = new Encrypter()
+const tokenGenerator = new TokenGenerator(env.secretKey)
+const authUsecase = new AuthUseCase({
+  loadUserByEmailRepository, updateAccessTokenRepository, encrypter, tokenGenerator
+})
+const emailValidator = new EmailValidator()
+const loginRoute = new LoginRoute({ authUsecase, emailValidator })
+
+module.exports = loginRoute
--- src/main/routes/login-routes.js
+++ src/main/routes/login-routes.js
@@ -1,21 +1,5 @@
-const LoginRouter = require('../../presentation/routes/login-route')
-const AuthUseCase = require('../../domain/usecases/auth-usecase')
-const EmailValidator = require('../../utils/helpers/email-validator')
-const Encrypter = require('../../utils/helpers/encrypter')
-const TokenGenerator = require('../../utils/helpers/token-generator')
-const LoadUserByEmailRepository = require('../../infra/repositories/load-user-by-email-repository')
-const UpdateAccessTokenRepository = require('../../infra/repositories/update-access-token-repository')
-const env = require('../config/env')
+const LoginRoute = require('../composers/login-route-composer')
 
 module.exports = router => {
-  const loadUserByEmailRepository = new LoadUserByEmailRepository()
-  const updateAccessTokenRepository = new UpdateAccessTokenRepository()
-  const encrypter = new Encrypter()
-  const tokenGenerator = new TokenGenerator(env.secretKey)
-  const authUsecase = new AuthUseCase({
-    loadUserByEmailRepository, updateAccessTokenRepository, encrypter, tokenGenerator
-  })
-  const emailValidator = new EmailValidator()
-  const loginRouter = new LoginRouter({ authUsecase, emailValidator })
-  router.post('/login', loginRouter)
+  router.post('/login', LoginRoute)
 }
